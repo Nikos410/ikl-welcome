5ad49c4170ad31c7f0b25df99338d99d724d4794
Nikos <nikos.epping@web.de>
2019-06-04 13:39:17 +0200

    Initial Import

---
+++ src/main/java/de/nikos410/ikl/welcome/IklWelcomeApplication.java
@@ -0,0 +1,13 @@
+package de.nikos410.ikl.welcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IklWelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IklWelcomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/nikos410/ikl/welcome/IklWelcomeApplicationTests.java
@@ -0,0 +1,16 @@
+package de.nikos410.ikl.welcome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IklWelcomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
