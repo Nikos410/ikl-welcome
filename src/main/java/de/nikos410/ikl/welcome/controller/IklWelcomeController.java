0b02c46075d2b90519253cee5636be90099f57b2
Nikos <nikos.epping@web.de>
2019-06-05 00:15:25 +0200

    Display random images from image directory

--- src/main/java/de/nikos410/ikl/welcome/controller/IklWelcomeController.java
+++ src/main/java/de/nikos410/ikl/welcome/controller/IklWelcomeController.java
@@ -1,12 +1,19 @@
 package de.nikos410.ikl.welcome.controller;
 
+import de.nikos410.ikl.welcome.service.ImageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IklWelcomeController {
+
+    private final ImageService imageService;
+
+    public IklWelcomeController(ImageService imageService) {
+        this.imageService = imageService;
+    }
 
     @GetMapping("")
     public String root() {
@@ -14,8 +21,13 @@ public class IklWelcomeController {
     }
 
     @GetMapping("/welcome")
-    public String welcome(Model model, @RequestParam("images") String images) {
-        model.addAttribute("images", images.split(";"));
+    public String welcome() {
         return "welcome";
+    }
+
+    @GetMapping("/nextimage")
+    @ResponseBody
+    public String nextImage() {
+        return imageService.getNextImageBase64();
     }
 }
