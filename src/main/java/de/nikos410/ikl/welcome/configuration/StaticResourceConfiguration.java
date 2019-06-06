e6b32a8e0ed8407f738cb6652bc4a0259de17d6d
Nikos410 <nikos.epping@web.de>
2019-06-06 21:03:36 +0200

    Fix typo

--- src/main/java/de/nikos410/ikl/welcome/configuration/StaticResourceConfiguration.java
+++ src/main/java/de/nikos410/ikl/welcome/configuration/StaticResourceConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.nio.file.Paths;
 
 @Configuration
-public class StaticResourceConficuration implements WebMvcConfigurer {
+public class StaticResourceConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
