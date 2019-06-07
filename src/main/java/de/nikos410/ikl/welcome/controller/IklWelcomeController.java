7c6aa27324ccb945e005e79eac96f417235b6a3c
Nikos <nikos.epping@web.de>
2019-06-08 00:54:39 +0200

    Remove unused controller endpoint

--- src/main/java/de/nikos410/ikl/welcome/controller/IklWelcomeController.java
+++ src/main/java/de/nikos410/ikl/welcome/controller/IklWelcomeController.java
@@ -4,10 +4,7 @@ import de.nikos410.ikl.welcome.model.Image;
 import de.nikos410.ikl.welcome.service.ImageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.nio.file.Path;
 
 @Controller
 public class IklWelcomeController {
@@ -32,11 +29,5 @@ public class IklWelcomeController {
     @ResponseBody
     public Image nextImage() {
         return imageService.getNextImage();
-    }
-
-    @GetMapping("/images/{fileName}/getinfo")
-    @ResponseBody
-    public String getInfoForImage(@PathVariable String fileName) {
-        return imageService.getInfoForImage(fileName);
     }
 }
